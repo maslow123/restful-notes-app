cb7574846a2d018ed701ab7ba60c67cfc38a6fbe
maslow123 <mfadhlyn@gmail.com>
2019-06-20 15:04:16 +0700

    there is change in the database

--- noteapp.sql
+++ noteapp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 20 Jun 2019 pada 08.57
+-- Generation Time: 20 Jun 2019 pada 10.03
 -- Versi Server: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -56,33 +56,29 @@ CREATE TABLE IF NOT EXISTS `description` (
   `category` int(11) NOT NULL,
   PRIMARY KEY (`desc_id`),
   KEY `category` (`category`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
 --
 -- Dumping data untuk tabel `description`
 --
 
 INSERT INTO `description` (`desc_id`, `title`, `note`, `time`, `category`) VALUES
-(1, 'My routine', 'I am very tired of all this, but I have to be even more enthusiastic', '0000-00-00 00:00:00', 1),
-(2, 'My holiday', 'Yesterday, i''m going to surabaya with my family', '0000-00-00 00:00:00', 3),
-(3, 'My diary', 'Hello, My name is M.Fadhly NR. You can call me Fadhly, i 17th years old ', '0000-00-00 00:00:00', 2),
-(4, 'My school', 'Iam have attended school in Fatahillah', '0000-00-00 00:00:00', 4),
-(5, 'vacationing at yogyakarta', 'I really like malioboro ', '0000-00-00 00:00:00', 3),
-(7, 'Yes we cant', 'everyone can be happy', '2019-06-20 04:01:30', 2),
-(8, 'not everyone has knowledge like this', 'but it''s all solved as long as everyone wants to try hard', '2019-06-20 04:01:30', 2),
-(12, 'What is Lorem Ipsum?', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2019-06-20 06:15:48', 1),
-(13, 'What is The meaning?', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2019-06-20 06:15:48', 1),
-(14, 'What is The Horse?', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2019-06-20 06:15:48', 4),
-(15, 'Why do we use it?', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', '2019-06-20 06:56:05', 1),
-(16, 'Where can I get some?', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don''t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn''t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', '2019-06-20 06:56:05', 2),
-(17, 'The standard Lorem Ipsum passage, used since the 1', '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."', '2019-06-20 06:56:05', 2),
-(18, 'Section 1.10.32 of "de Finibus Bonorum et Malorum"', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet', '2019-06-20 06:56:05', 3),
-(19, 'consectetur, adipisci velit, sed quia non numquam ', 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"', '2019-06-20 06:56:05', 4),
-(20, 'Why do we use it?', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ''Content here, content here'', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ''lorem ipsum'' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', '2019-06-20 06:56:15', 1),
-(21, 'Where can I get some?', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don''t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn''t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', '2019-06-20 06:56:15', 2),
-(22, 'The standard Lorem Ipsum passage, used since the 1', '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."', '2019-06-20 06:56:15', 2),
-(23, 'Section 1.10.32 of "de Finibus Bonorum et Malorum"', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet', '2019-06-20 06:56:15', 3),
-(24, 'consectetur, adipisci velit, sed quia non numquam ', 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"', '2019-06-20 06:56:15', 4);
+(3, 'vacationing at yogyakarta', 'I really like malioboro ', '2019-06-20 07:49:29', 3),
+(4, 'vacationing at jakarta', 'I really like monas ', '2019-06-20 07:49:46', 3),
+(5, 'vacationing at bogor', 'I really like palace bogor ', '2019-06-20 07:50:05', 3),
+(6, 'vacationing at bandung', 'I really like gedung sate ', '2019-06-20 07:50:24', 3),
+(7, 'vacationing at kediri', 'I really like agung mosque', '2019-06-20 07:50:47', 3),
+(8, 'vacationing at malaysia', 'I really like kuala lumpur', '2019-06-20 07:51:23', 3),
+(9, 'Notes app project', 'This week i do make a simple project that is restful API with node JS', '2019-06-20 07:52:34', 1),
+(10, 'Learning Node JS', 'learn node js is very exciting, and make a head dizzy LOL', '2019-06-20 07:53:35', 1),
+(11, 'My Name', 'My Name is M.Fadhly NR, you can call me Fadhly or other', '2019-06-20 07:54:37', 4),
+(12, 'My Name', 'My Name is M.Fadhly NR, you can call me Fadhly or other', '2019-06-20 07:54:42', 4),
+(13, 'My Education', 'My last education is vocational school', '2019-06-20 07:55:36', 4),
+(14, 'I was a freelancer', 'My last project is make a some project for mosque, that programs name is Takmir Masjid', '2019-06-20 07:56:38', 4),
+(15, 'I have a motto', 'That are if you want to be success, you have to feel the hard feeling of learning', '2019-06-20 07:58:40', 4),
+(16, 'My family', 'Iam the second of five children', '2019-06-20 07:59:49', 2),
+(17, 'My Hobbies', 'I have are hobbies, for example reading and trying new things', '2019-06-20 08:00:43', 2),
+(18, 'Become a developer is my goals !', 'Because i have my inspiration, for example is Peter Jack Kambey', '2019-06-20 08:02:01', 2);
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
